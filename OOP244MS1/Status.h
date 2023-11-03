142ec64bbd9bc31225bcc7f87221f7eae36a05f4
arnob-rafi <arnobbkash@gmail.com>
2023-11-02 21:54:58 -0400

    oop-244 ms1

---
+++ OOP244MS1/Status.h
@@ -0,0 +1,31 @@
+//rafi islam arnob
+//ID-118211226
+//OOP244ZCC
+
+
+#ifndef SDDS_STATUS_H
+#define SDDS_STATUS_H
+#include <iostream>
+#include<cstring>
+namespace sdds {
+    class Status
+    {
+        char* m_description{};
+        int m_code;
+    public:
+        Status(const char* description = nullptr);
+        Status(const Status& stat);
+        Status& operator =(const char* description);
+        Status& operator =(int code);
+        operator int() const;
+        operator const char* () const;
+        operator bool() const;
+        Status& clear();
+        std::ostream& print(std::ostream& ostr) const;
+        ~Status();
+
+    };
+    std::ostream& operator<<(std::ostream& os, const Status& stat);
+
+}
+#endif
